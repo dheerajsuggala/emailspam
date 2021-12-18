e7aa30f7b5ee0322b7063a2fce4d5a90cb37cd47
dheerajsuggala <56378524+dheerajsuggala@users.noreply.github.com>
2021-12-18 19:31:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import pickle
+import dill
 
 
 app = Flask(__name__)
@@ -9,25 +9,20 @@ app = Flask(__name__)
 def emailsspam():
     if request.method == 'POST':
         text = request.form['email_body']
-        with open('emailspam.pkl', 'rb') as file:
-            classifier = pickle.load(file)
-        with open('vector.pkl', 'rb') as file:
-            vector = pickle.load(file)
-        return 'TRUE'
+        with open('vector.joblib','rb') as io:
+            vector=dill.load(io)
+        with open('classifier.joblib','rb') as io:
+            classifier=dill.load(io)
+        input_text = vector.transform(text)
+        result = classifier.predict(input_text)
+        if str(result) == '1':
+            return render_template('emailsspam.html', status='danger', result='Spam')
+        else:
+            return render_template('emailsspam.html', status='success', result='Not Spam')   
     
     return render_template('emailsspam.html')
 
-@app.route('/b')
-def bb():
-    with open('var.pkl', 'rb') as file:
-        b = pickle.load(file)
-    return str(b)
 
-@app.route('/app')
-def cc():
-    with open('varr.pkl', 'rb') as file:
-        b = pickle.load(file)
-    return str(b)
 
 if __name__ == '__main__':
     app.run(debug=True)
