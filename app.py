203ed2ef22d2318ebfbdf993dd0cda3a2e31aa66
dheerajsuggala <56378524+dheerajsuggala@users.noreply.github.com>
2021-12-18 17:52:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,20 +3,19 @@ import pickle
 
 
 app = Flask(__name__)
-# classifier = joblib.load('emailspam.pkl')
-# vector = joblib.load('vector.pkl')
 
-# with open('emailspam.pkl', 'rb') as file:
-#     classifier = pickle.load(file)
 
-# with open('vector.pkl', 'rb') as file:
-#     vector = pickle.load(file)
+
 
 
 @app.route('/', methods=['POST', 'GET'])
 def emailsspam():
     if request.method == 'POST':
         text = request.form['email_body']
+        with open('emailspam.pkl', 'rb') as file:
+            classifier = pickle.load(file)
+        with open('vector.pkl', 'rb') as file:
+            vector = pickle.load(file)
 #         input_text = vector.transform(text)
 #         result = classifier.predict(input_text)
 #         if str(result) == '1':
