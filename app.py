90d8718c0fa8e487888e6dd8a0b2a949fae5bb96
dheerajsuggala <56378524+dheerajsuggala@users.noreply.github.com>
2021-12-18 15:50:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 from flask import Flask, render_template, request
-import joblib
+# import joblib
 
 
 app = Flask(__name__)
-classifier = joblib.load('emailspam.pkl')
-vector = joblib.load('vector.pkl')
+# classifier = joblib.load('emailspam.pkl')
+# vector = joblib.load('vector.pkl')
 
 
 @app.route('/', methods=['POST', 'GET'])
 def emailsspam():
     if request.method == 'POST':
         text = request.form['email_body']
-        input_text = vector.transform(text)
-        result = classifier.predict(input_text)
-        if str(result) == '1':
-            return render_template('emailsspam.html', status='danger', result='Spam')
-        else:
-            return render_template('emailsspam.html', status='success', result='Not Spam')
+#         input_text = vector.transform(text)
+#         result = classifier.predict(input_text)
+#         if str(result) == '1':
+#             return render_template('emailsspam.html', status='danger', result='Spam')
+#         else:
+#             return render_template('emailsspam.html', status='success', result='Not Spam')
+        return text
     return render_template('emailsspam.html')
 
 
