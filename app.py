951d91b7e5ce7e8a374d48a1b8de9f23f043dda3
dheerajsuggala <56378524+dheerajsuggala@users.noreply.github.com>
2021-12-18 19:56:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,13 +12,17 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def emailsspam():
     if request.method == 'POST':
+        print('------------------>>>>>>1')
         text = request.form['email_body']
         with open('vector.joblib','rb') as io:
             vector=dill.load(io)
         with open('classifier.joblib','rb') as io:
             classifier=dill.load(io)
+        print('------------------>>>>>>2')
         input_text = vector.transform([text])
+        print('------------------>>>>>>3')
         result = classifier.predict(input_text)
+        print('------------------>>>>>>',str(result[0]))
         if str(result[0]) == '1':
             return render_template('emailsspam.html', status='danger', result='Spam')
         else:
